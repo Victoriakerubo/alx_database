188cb8d8b97b1c3390ae2a68a3e43dd88920360a
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-21 11:56:54 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,6 @@
 -- 3-list_tables.sql
 
 -- List all tables in the specified database
-USE your_database_name;  -- Replace "your_database_name" with the actual database name
+USE hbtn_0c_0;  -- Replace with the actual database name
 SHOW TABLES;
 
