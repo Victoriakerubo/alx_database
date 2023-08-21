29d06bf31a961ddd35b9cc9f7dfe6d99cbc1da84
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-21 12:15:20 +0300

    commit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,7 +2,10 @@
 
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) DEFAULT NULL,
+    c CHAR(1) DEFAULT NULL,
+    created_at DATE DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
