72de6f8870ac3ba1e2c8c7b1106bf4c33a4b01b1
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-22 18:14:36 +0300

    commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS force_name (
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
+-- Inserting data into the force_name table
+
+-- Inserting the first row
+INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
+
+-- Inserting additional rows
+INSERT INTO force_name (id, name) VALUES (2, 'Python is cool');
+INSERT INTO force_name (id, name) VALUES (3, 'Holberton');
+INSERT INTO force_name (id, name) VALUES (4, 'School');
+INSERT INTO force_name (id, name) VALUES (5, 'C is fun');
