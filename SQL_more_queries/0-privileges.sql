150567b99ed61423cb47f5445245a516689bca15
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-22 16:33:48 +0300

    commit

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- Connect to MySQL server and create users
+CREATE USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_2'@'localhost';
+
+-- Grant privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
