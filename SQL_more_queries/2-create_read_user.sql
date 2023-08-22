01e05c3a7711e58197582a66ba0453c3e6cfb52d
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-22 18:08:23 +0300

    commit

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- 2-create_read_user.sql
+
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
