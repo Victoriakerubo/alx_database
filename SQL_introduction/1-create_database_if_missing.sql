fa45a56ae30f1753ab16af0e18e079ce8267fa3b
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-25 17:33:01 +0300

    commit

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,9 @@
--- 1-create_database_if_missing.sql
-
--- Create the database only if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
